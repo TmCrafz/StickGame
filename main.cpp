1e2c6179c0ab29d8d6a2a0f8cf2d963223aec760
Tim <sterzeltim@gmail.com>
2017-01-23 19:38:36 +0100

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+
+int main()
+{
+	// The hight of the stack of sticks
+	int stackHeight{ 10 };
+	// At the beginning both stacks have the same hight
+	int leftStack{ stackHeight };
+	int rightStack{ stackHeight };
+
+
+}
