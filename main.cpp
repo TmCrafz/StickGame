2aadcca1b141e3eed5fd470b510b374bfc0f9c55
Tim <sterzeltim@gmail.com>
2017-01-23 20:36:59 +0100

    Start implementing gameplay

--- main.cpp
+++ main.cpp
@@ -4,7 +4,21 @@
 int main()
 {
 	StickStack stickStack{ 10 };
-	stickStack.removeFromLeft();
-	stickStack.removeFromBoth();
 	stickStack.printStack();
+	bool isRunning{ true };
+	while(isRunning)
+	{
+		std::cout << "[l] Remove from left [r] remove from right [b] Remove from both" << std::endl;
+		char input{ ' ' };
+		std::cin >> input;
+		std::cout << std::endl;
+		switch(input)
+		{
+			case 'l' : stickStack.removeFromLeft(); break;
+			case 'r' : stickStack.removeFromRight(); break;
+			case 'b' : stickStack.removeFromBoth(); break;
+			default: isRunning = false;
+		}
+		stickStack.printStack();
+	}
 }
