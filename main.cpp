b6f863bfdd5f584a33f022ba5fd3e8e829ea6748
TmCrafz <sterzeltim@gmail.com>
2017-01-26 21:21:29 +0100

    Clean up

--- main.cpp
+++ main.cpp
@@ -1,13 +1,10 @@
 #include <iostream>
 #include "include/Game.hpp"
 #include "include/StickStack.hpp"
-#include "include/AI.hpp"
 #include <array>
 
 int main()
 {
-	AI ai{ 10 };
-	ai.printTable();
 	Game game{ 10 };
 	game.run();
 }
